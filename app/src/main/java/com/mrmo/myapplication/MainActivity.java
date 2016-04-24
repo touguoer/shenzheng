c4b1ccf5736aae51c616053c1dcc399930bc055b
MrMo <1558286660@qq.com>
2016-04-24 16:07:27 +0800

    第一次使用git提交项目

--- app/src/main/java/com/mrmo/myapplication/MainActivity.java
+++ app/src/main/java/com/mrmo/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void get(){}
+    public void get(){
+        int i=20;
+    }
 }
